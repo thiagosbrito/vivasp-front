52de914b824f6b1d6c08eedbb8889c718872015a
Thiago Brito <thiagosbrito@thiagosbrito.local>
2017-05-30 23:30:12 -0300

    CI - test

--- gulpfile.js
+++ gulpfile.js
@@ -27,3 +27,6 @@ wrench.readdirSyncRecursive('./gulp').filter(function(file) {
 gulp.task('default', ['clean'], function () {
   gulp.start('build');
 });
+gulp.task('heroku:production', ['build'], function(){
+  console.log('herokuduction');
+});
